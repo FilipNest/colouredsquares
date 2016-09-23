42ef878fd2305750aa369d6660536ce69b10807a
Filip Hnízdo <filip@bluejumpers.com>
2016-09-23 16:13:06 +0100

    getColours function and update

--- public/client.js
+++ public/client.js
@@ -2,6 +2,28 @@
 
 var sliders = [document.getElementById("slide-red"), document.getElementById("slide-blue"), document.getElementById("slide-green")];
 
+var getColours = function () {
+
+  var red = document.getElementsByName("red")[0].value;
+  var green = document.getElementsByName("green")[0].value;
+  var blue = document.getElementsByName("blue")[0].value;
+
+  return {
+    red: red,
+    green: green,
+    blue: blue
+  }
+
+}
+
+var updateColour = function () {
+
+  var colours = getColours();
+
+  document.getElementById("current").style.backgroundColor = "rgb(" + colours.red + "," + colours.green + "," + colours.blue + ")";
+
+}
+
 if (sliders[0].type === "range") {
 
   for (var i = 0; i < sliders.length; i += 1) {
@@ -12,6 +34,8 @@ if (sliders[0].type === "range") {
       var colour = document.getElementsByName(e.target.name.replace("slide-", ""))[0];
 
       colour.value = value;
+
+      updateColour();
 
     })
 
