10d774dd71c6f4a75733d6c1e29849a642d5105f
Filip Hnízdo <filip@bluejumpers.com>
2016-09-23 14:24:32 +0100

    Detect range input support and show

---
+++ public/client.js
@@ -0,0 +1,25 @@
+// Check if browser supports range input type
+
+var sliders = [document.getElementById("slide-red"), document.getElementById("slide-blue"), document.getElementById("slide-green")];
+
+if (sliders[0].type === "range") {
+
+  for (var i = 0; i < sliders.length; i += 1) {
+
+    sliders[i].addEventListener("change", function (e) {
+
+      var value = e.target.value;
+      var colour = document.getElementsByName(e.target.name.replace("slide-", ""))[0];
+
+      colour.value = value;
+
+    })
+
+  }
+
+  // Range type supported, enable range type
+
+  document.getElementById("sliders").className += " visible";
+
+
+}
