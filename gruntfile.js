319c34da8d602f5c4e4fbf6f2a752c55d23c7e67
Filip Hnízdo <filip@bluejumpers.com>
2016-09-28 22:25:55 +0100

    Grunt watch

--- gruntfile.js
+++ gruntfile.js
@@ -21,11 +21,23 @@ module.exports = function (grunt) {
           'public/style.min.css': ['public/style.css']
         }
       }
-    }
+    },
+    watch: {
+      files: ['public/client.js', 'public/style.css'],
+      tasks: ['default'],
+      options: {
+        spawn: false
+      }
+    },
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.registerTask('default', ['cssmin', 'uglify']);
+
+  grunt.event.on('watch', function (action, filepath, target) {
+    grunt.log.writeln(target + ': ' + filepath + ' has ' + action);
+  });
 
 };
